b2366e63c76a5ff4cf7652e40ee0903f66138293
cladams0203 <cladams0203@gmail.com>
2019-10-09 22:48:08 -0400

    finished mvp with darkMode hook and useLocalStorage hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
+
+export function useDarkMode(value) {
+    const [darkMode, setDarkMode] = useLocalStorage(value)
+    useEffect(() => {
+        const body = document.querySelector('body')
+        console.log(body)
+        darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
+    },[darkMode])
+    return [darkMode, setDarkMode]
+}
