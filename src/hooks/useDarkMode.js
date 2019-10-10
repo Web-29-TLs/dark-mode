fab1ea4f446138355b0d2c2fa102cf28c9577ac1
cladams0203 <cladams0203@gmail.com>
2019-10-09 22:58:30 -0400

    did some cleanup

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,7 +5,6 @@ export function useDarkMode(value) {
     const [darkMode, setDarkMode] = useLocalStorage(value)
     useEffect(() => {
         const body = document.querySelector('body')
-        console.log(body)
         darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
     },[darkMode])
     return [darkMode, setDarkMode]
